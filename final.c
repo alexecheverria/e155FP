2efd03657e9ecc9bb9231679f35d9741d1575c14
alexecheverria <aecheverria@g.hmc.edu>
2017-12-03 01:05:49 -0800

    added final.c

--- final.c
+++ final.c
@@ -1,56 +1,117 @@
-#include "spi.h"
- 
- void  main(void){
-   int output;
-   int fclk;
-   float vin;
-   int vdd=5;
-   int out;
+#include <stdio.h>
+
+
+void updatePins(int timeScale[], int newNumber,int dataPi,int evenOrOdd){
+	   //TimeScale Pins
+	   pinMode(4,INPUT); 
+       pinMode(17,INPUT); 
+       pinMode(27,INPUT); 
+       pinMode(22,INPUT); 
+       pinMode(5,INPUT); 
+       pinMode(6,INPUT);
+        
+       pinMode(24,INPUT); //newNumber
+       pinMode(18,INPUT); //dataPi
+       pinMode(23,INPUT); //evenOrOdd
+
+	   timeScale[0] = digitalRead(4);
+	   timeScale[1] = digitalRead(17);
+	   timeScale[2] = digitalRead(27);
+	   timeScale[4] = digitalRead(22);
+	   timeScale[5] = digitalRead(5);
+	   timeScale[6] = digitalRead(6);
+
+
+       newNumber = digitalRead(24);
+       dataPi = digitalRead(18);
+       evenOrOdd = digitalRead(23);
+
+}
+
+
+ int  main(){
+   
+
 
    /*Intialize PIO*/
    pioInit();
-
-   fclk = 122000; // ADC has 3.2MHz clk at 5V
-
-   /*Voltage Scale Logic*/
-   //TODO: Determine what voltage scales we want. Max input 5 Volts
-   bool voltScale[3]; // [x volts/div, y volts div, 1 volt/div]
-   pinMode(16,INPUT); //x volts/div
-   pinMode(20,INPUT); //y volts/div
-   pinMode(21,INPUT); //1 volt/div
-  
-   voltsScale[0] = digitalRead(16);
-   voltsScale[1] = digitalRead(20);
-   voltsScale[2] = digitalRead(21);
+   
 
    /*Time Scale Logic*/
-   bool timeScale[3]; // [x volts/div, y volts div, 1 volt/div]
-   pinMode(13,INPUT); //x volts/div
-   pinMode(19,INPUT); //y volts/div
-   pinMode(26,INPUT); //1 volt/div
-  
-   timeScale[0] = digitalRead(13);
-   timeScale[1] = digitalRead(19);
-   timeScale[2] = digitalRead(26);
+   bool timeScale[6]; // [x volts/div, y volts div, 1 volt/div
+ 
 
-   /*Initialize SPI pins*/
-   spiInit(fclk, 00); // set both phase and pol to 0
+   bool evenOrOdd;
+   bool nextBit;
+   bool dataPi;
+   bool newNumber;
 
 
-   //OLD ADC CODE
-   /* Retreive data from the ADC*/
-   int out1 = (int)  SPIsendReceive(0b01101000);
-   int out2 = (int) SPIsendReceive(0b00000000);
-   out1 &= 0b11; // get rid of first 6 bits
-   out1 = out1 << 8;
-   out = out1 | out2;
-   // convert digital output code to voltage
-   vin = (float) out * vdd / 1024;
+   float dataToPlot;
+   float timeToPlot;
+   float conversionFactor = 5/4096;
 
-   printf("%s%c%c\n", "Content-Type: text/html;charset=iso-8859-1",13,10);
-   printf("<META HTTP-EQUIV=\"Refresh\" CONTENT=\"0;url=/cgi-bin/lab6\">");
-   printf("<b>The light sensor's voltage is: %f </b><p>", vin);
-   printf("<i>Press the back button to return.</i>\n\n");
+   int counter = 12;
+   int timeCount = 0;
+   bool timeCountTrue = 0;
 
-   return;
- }
+
+   float delta t0.0000128;
+
+   while(true){
+   	   
+   	 
+   	   //log data in .data
+   	  while(timeCountTrue){
+   	  		
+	   	  		  //get data from FPGA
+	   	   for(counter = 12; counter > 0, counter = counter-1){
+	   	   		updatePins(timeScale,voltsScale,newNumber,dataPi,evenOrOdd);
+	   	   		//Turn serial data to number
+	   	   		dataToPlot = (2**(counter+1)*dataPi)*conversionFactor;
+	   			nextBit = evenOrOdd
+	   			//Wait for next bit
+	   			while(nextBit == evenOrOdd){
+	   				//update new pins
+	   				updatePins(timeScale,newNumber,dataPi,evenOrOdd);
+	   			}
+	   	  }
+	   }
+
+   	  	  //Check timeScale
+	   	  if(timeScale[6] == 1){
+	   	  	timeCountTrue = (timeCount == 78125);
+	   	  }
+	   	  else if(timeScale[5] == 1){
+	   	  	timeCountTrue = !(timeCount == 78125);
+	   	  }
+	   	  else if(timeScale[4] == 1){
+	   	  	timeCountTrue = !(timeCount == 7813);
+	   	  }
+	   	  else if(timeScale[3] == 1){
+	   	  	timeCountTrue = !(timeCount == 792);
+	   	  }
+	   	  else if(timeScale[2] == 1){
+	   	  	timeCountTrue = !(timeCount == 78);
+	   	  }
+	   	  else if(timeScale[1] == 1){
+	   	  	timeCountTrue = !(timeCount == 8);
+	   	  }
+	   	  else if(timeScale[0] == 1){
+			timeCountTrue = !(timeCount == 1);
+
+			timeToPlot = timeCount*deltaT;
+			timeCount = timeCount + 1;
+			FILE * fp;
+			/* open the file for writing*/
+			fp = fopen ("values.data","a");
+			fprintf (fp, "%f %f\n",timeToPlot,dataToPlot);
+			/* close the file*/  
+			fclose (fp);
+   	  
+   }
+
+
+
+   return 0;
+}
